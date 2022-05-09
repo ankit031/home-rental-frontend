082a7669f95f3c1f2af875379ed0288c01e7e82b
ankits031 <ankit@socialwell.net>
2022-05-09 22:49:36 +0530

    initial commit

---
+++ login.js
@@ -0,0 +1,50 @@
+let jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+  window.location.href = './index.html'
+}
+
+function login() {
+    console.log("Inside login function");
+  const username = document.getElementById("email").value;
+  console.log("username ",username);
+
+  const password = document.getElementById("password").value;
+  console.log("password ",password);
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://localhost:4001/v1/auth/login");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({
+    "email": username,
+    "password": password
+  }));
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log("objects ",objects);
+      if (objects['user']) {
+          console.log("inside objects['status'] ",objects['status'])
+        localStorage.setItem("jwt", objects['tokens']['access']['token']);
+        console.log("objects['tokens']['access']['token'] ",objects['tokens']['access']['token']);
+        window.location.href = './index.html';
+
+        // Swal.fire({
+        //   text: objects['message'],
+        //   icon: 'success',
+        //   confirmButtonText: 'OK'
+        // }).then((result) => {
+        //   if (result.isConfirmed) {
+        //     window.location.href = './index.html';
+        //   }
+        // });
+      } else {
+        // Swal.fire({
+        //   text: objects['message'],
+        //   icon: 'error',
+        //   confirmButtonText: 'OK'
+        // });
+      }
+    }
+  };
+  return false;
+}
